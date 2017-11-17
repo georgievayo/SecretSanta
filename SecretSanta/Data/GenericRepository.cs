e5be9298abdf6bfdd233693776552f97532d0212
Yoana Georgieva <yo.ana.georgieva@abv.bg>
2017-11-17 17:58:45 +0200

    Fix repository and unit of work

--- SecretSanta/Data/GenericRepository.cs
+++ SecretSanta/Data/GenericRepository.cs
@@ -7,44 +7,39 @@ namespace SecretSantaData
     public class GenericRepository<T>: IRepository<T>
         where T : class
     {
-        private readonly SecretSantaDbContext dbContext;
+        private readonly SecretSantaDbContext _dbContext;
 
         public GenericRepository(SecretSantaDbContext dbContext)
         {
-            if (dbContext == null)
-            {
-                throw new ArgumentNullException(nameof(dbContext));
-            }
-
-            this.dbContext = dbContext;
+            this._dbContext = dbContext;
         }
 
         public IQueryable<T> All
         {
             get
             {
-                return this.dbContext.DbSet<T>();
+                return this._dbContext.DbSet<T>();
             }
         }
 
         public void Add(T entity)
         {
-            this.dbContext.SetAdded(entity);
+            this._dbContext.SetAdded(entity);
         }
 
         public void Delete(T entity)
         {
-            this.dbContext.SetDeleted(entity);
+            this._dbContext.SetDeleted(entity);
         }
 
         public T GetById(object id)
         {
-            return this.dbContext.DbSet<T>().Find(id);
+            return this._dbContext.DbSet<T>().Find(id);
         }
 
         public void Update(T entity)
         {
-            this.dbContext.SetUpdated(entity);
+            this._dbContext.SetUpdated(entity);
         }
     }
 }
--- SecretSanta/Data/UnitOfWork.cs
+++ SecretSanta/Data/UnitOfWork.cs
@@ -5,21 +5,16 @@ namespace SecretSantaData
 {
     public class UnitOfWork : IUnitOfWork
     {
-        private readonly SecretSantaDbContext dbContext;
+        private readonly SecretSantaDbContext _dbContext;
 
         public UnitOfWork(SecretSantaDbContext dbContext)
         {
-            if (dbContext == null)
-            {
-                throw new ArgumentNullException(nameof(dbContext));
-            }
-
-            this.dbContext = dbContext;
+            this._dbContext = dbContext;
         }
 
         public void SaveChanges()
         {
-            this.dbContext.SaveChanges();
+            this._dbContext.SaveChanges();
         }
     }
 }
